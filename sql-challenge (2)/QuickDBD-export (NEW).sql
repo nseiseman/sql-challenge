221513ec689ecb19f3209cb78021e838dd108588
nseiseman <131634878+nseiseman@users.noreply.github.com>
2024-01-22 21:37:58 -0500

    Add files via upload

---
+++ sql-challenge (2)/QuickDBD-export (NEW).sql
@@ -0,0 +1,75 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE `departments` (
+    `dept_no` VARCHAR(4)  NOT NULL ,
+    `dept_name` VARCHAR  NOT NULL ,
+    PRIMARY KEY (
+        `dept_no`
+    )
+);
+
+CREATE TABLE `dept_emp` (
+    `emp_no` BIGINT  NOT NULL ,
+    `dept_no` VARCHAR(4)  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+CREATE TABLE `dept_manager` (
+    `dept_no` VARCHAR(4)  NOT NULL ,
+    `emp_no` BIGINT  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+CREATE TABLE `titles` (
+    `title_id` VARCHAR(5)  NOT NULL ,
+    `title` VARCHAR  NOT NULL ,
+    PRIMARY KEY (
+        `title_id`
+    )
+);
+
+CREATE TABLE `employee` (
+    `emp_no` BIGINT  NOT NULL ,
+    `title_id` VARCHAR(5)  NOT NULL ,
+    `birth_date` DATE  NOT NULL ,
+    `first_name` VARCHAR  NOT NULL ,
+    `last_name` VARCHAR  NOT NULL ,
+    `sex` VARCHAR(1)  NOT NULL ,
+    `hire_date` DATE  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+CREATE TABLE `salaries` (
+    `emp_no` BIGINT  NOT NULL ,
+    `salary` BIGINT  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+ALTER TABLE `dept_emp` ADD CONSTRAINT `fk_dept_emp_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `employee` (`emp_no`);
+
+ALTER TABLE `dept_emp` ADD CONSTRAINT `fk_dept_emp_dept_no` FOREIGN KEY(`dept_no`)
+REFERENCES `departments` (`dept_no`);
+
+ALTER TABLE `dept_manager` ADD CONSTRAINT `fk_dept_manager_dept_no` FOREIGN KEY(`dept_no`)
+REFERENCES `departments` (`dept_no`);
+
+ALTER TABLE `dept_manager` ADD CONSTRAINT `fk_dept_manager_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `employee` (`emp_no`);
+
+ALTER TABLE `employee` ADD CONSTRAINT `fk_employee_title_id` FOREIGN KEY(`title_id`)
+REFERENCES `titles` (`title_id`);
+
+ALTER TABLE `salaries` ADD CONSTRAINT `fk_salaries_emp_no` FOREIGN KEY(`emp_no`)
+REFERENCES `employee` (`emp_no`);
+
---
+++ sql-challenge (2)/sql-challenge (NEW).sql
@@ -0,0 +1,164 @@
+Dd--Create Tables
+CREATE TABLE departments (
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR NOT NULL, 
+	PRIMARY KEY (dept_no) 
+);
+
+CREATE TABLE dept_emp (
+    emp_no BIGINT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no BIGINT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(5) NOT NULL,
+    title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees (
+    emp_no BIGINT NOT NULL,
+    title_id VARCHAR(5) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no),
+	FOREIGN KEY (title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE salaries (
+    emp_no BIGINT NOT NULL,
+    salary BIGINT NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+--
+
+-- View Data
+SELECT * FROM departments;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM titles;
+--
+
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT
+    emp.emp_no,
+    emp.last_name,
+    emp.first_name,
+    emp.sex,
+    sal.salary
+FROM
+    employees emp
+JOIN
+    salaries sal ON emp.emp_no = sal.emp_no;
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT
+    emp.first_name,
+    emp.last_name,
+    emp.hire_date
+FROM
+    employees emp
+WHERE
+    EXTRACT(YEAR FROM emp.hire_date) = 1986; -- https://www.sqltutorial.org/sql-date-functions/how-to-extract-year-from-date-in-sql/
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT
+	manager.dept_no,
+	dept.dept_name,
+	manager.emp_no,
+	emp.last_name,
+    emp.first_name	
+FROM
+	dept_manager manager	
+JOIN
+	departments dept ON manager.dept_no = dept.dept_no
+JOIN	
+	employees emp ON manager.emp_no = emp.emp_no;
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT
+	dept_emp.dept_no,
+	emp.emp_no,
+	emp.last_name,
+    emp.first_name,
+	dept.dept_name
+FROM
+	employees emp
+JOIN
+	dept_emp dept_emp ON emp.emp_no = dept_emp.emp_no
+JOIN	
+	departments dept ON dept_emp.dept_no = dept.dept_no;
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT
+	emp.first_name,
+	emp.last_name,
+	emp.sex
+FROM
+	employees emp
+WHERE
+	emp.first_name = 'Hercules'
+AND
+	LEFT(emp.last_name, 1) = 'B';
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT
+	emp.emp_no,
+	emp.last_name,
+    emp.first_name
+FROM
+	employees emp
+JOIN
+	dept_emp dept_emp ON emp.emp_no = dept_emp.emp_no
+JOIN	
+	departments dept ON dept_emp.dept_no = dept.dept_no
+WHERE
+	dept.dept_name = 'Sales'
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT
+	emp.emp_no,
+	emp.last_name,
+    emp.first_name
+FROM
+	employees emp
+JOIN
+	dept_emp dept_emp ON emp.emp_no = dept_emp.emp_no
+JOIN	
+	departments dept ON dept_emp.dept_no = dept.dept_no
+WHERE
+	dept.dept_name IN ('Sales', 'Development');
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT
+	last_name,
+	COUNT(last_name)
+	AS frequency
+FROM
+	employees
+GROUP BY
+	last_name
+ORDER BY
+	frequency DESC;
